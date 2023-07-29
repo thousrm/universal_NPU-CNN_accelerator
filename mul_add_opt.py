42d3bffb13b71e15069ce61910cdeb5bc7e310e6
thousrm <thousrm@korea.ac.kr>
2023-07-29 13:08:17 +0900

    update

--- mul_add_opt.py
+++ mul_add_opt.py
@@ -1,52 +1,88 @@
+"""
+
+get optimized process of wallace tree
+
+"""
+
 import numpy as np
 import copy
 
 original = np.array([0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1])
-list_num = []
 
+list_num = []
 types = [5, 4, 3]
-area = [3, 2, 1]
+area = [300, 200, 1]
 delay = [3, 2, 1]
 best_cost = 1000000
 best_series = []
+step = 0
 
 
 def getopt(line, cost, wa, wd):
+    global best_cost, best_series, list_num, step
 
-    global best_cost, best_series
+    step = step + 1
+    print("getopt{}".format(step))
 
     for i in [0, 1, 2]:
         temp = copy.deepcopy(line)
-        np.append(temp, types[i])
+        list_num.append(types[i])
         rate = temp // types[i]
         remain = temp % types[i]
-        cost_temp = cost + sum(rate) * area[i] * wa + delay[i] * wd
+        cost_temp = cost + max(sum(rate), 1) * area[i] * wa + delay[i] * wd
 
         temp = rate + remain
         rate = np.delete(rate, 0)
         rate = np.append(rate, 0)
         temp = temp + rate
 
+        if i == 0 or i == 1:
+            rate = np.delete(rate, 0)
+            rate = np.append(rate, 0)
+            temp = temp + rate
+
+        print("cost_temp: {}".format(cost_temp))
+        print("list: {}".format(list_num))
+        print("temp: {}".format(temp))
+        print("best_cost: {}".format(best_cost))
+        print("best_series: {}".format(best_series))
+
         temp_true = temp < types[i]
 
         if all(temp_true):
-            getopt3(temp, cost_temp, wa, wd)
+            print("true\n")
+            [cost_temp, time] = getopt3(temp, cost_temp, wa, wd)
             if cost_temp < best_cost:
                 best_cost = cost_temp
-                best_series = copy.deepcopy(temp)
-                np.pop(temp)
-        else:
-            if cost_temp > best_cost:
-                np.pop(temp)
-
+                best_series = copy.deepcopy(list_num)
+                j = 0
+                while j <= time:
+                    list_num.pop()
+                    j = j + 1
+                    ##print("list: {}".format(list_num))
+                print("best_cost: {}".format(best_cost))
+                print("best_series: {}\n".format(best_series))
             else:
+                j = 0
+                while j <= time:
+                    list_num.pop()
+                    j = j + 1
+        else:
+            print("false\n")
+            if cost_temp < best_cost:
                 getopt(temp, cost_temp, wa, wd)
+                list_num.pop()
+            else:
+                list_num.pop()
 
 
 def getopt3(line, cost, wa, wd):
+    global list_num
+
     temp = copy.deepcopy(line)
     temp_true = temp < 3
     cost_temp = cost
+    time = 0
 
     while not all(temp_true):
         rate = temp // 3
@@ -56,12 +92,17 @@ def getopt3(line, cost, wa, wd):
         rate = np.delete(rate, 0)
         rate = np.append(rate, 0)
         temp = temp + rate
+        list_num.append(3)
+        time = time + 1
         temp_true = temp < 3
+        ##print("temp: {}".format(temp))
+        ##print("time: {}".format(time))
 
-    return [temp, cost_temp]
+    return [cost_temp, time]
 
-##A = getopt3(original, 0, 0, 1)
-##print("{}".format(A))
+
+getopt(original, 0, 1, 0)
+print("{}, {}".format(best_cost, best_series))
 
 """
 
