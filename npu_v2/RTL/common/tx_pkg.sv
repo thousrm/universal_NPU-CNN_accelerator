e3bce94e7260273f54ae4b492243ec52255e6995
thousrm <thousrm@korea.ac.kr>
2024-10-01 10:44:57 +0900

    fix fp32 adder
    .

--- npu_v2/RTL/common/tx_pkg.sv
+++ npu_v2/RTL/common/tx_pkg.sv
@@ -3,17 +3,27 @@ package tx_pkg;
 
 import mac_pkg::*;
 
+parameter W_DATATYPE = 3;
+
+typedef struct packed {
+    logic   [W_DATATYPE   -1:0]     ifm_datatype;
+    logic   [W_DATATYPE   -1:0]     wfm_datatype;
+    logic                           bias_enable ; 
+    logic                           bias_mode   ; 
+    
+} mac_instruction_port;
+
 typedef struct packed {
     logic   [MAC_W_ELEMENT*64-1:0]  data;
-    logic   [64  -1:0]  data_element_valid;
-    logic               inter_end;
-    logic               accum_end;
+    logic   [64  -1:0]              data_element_valid;
+    logic                           inter_end;
+    logic                           accum_end;
 } mac_ifm_port;
 
 typedef struct packed {
     logic   [MAC_W_ELEMENT*64-1:0]  data;
     //logic   [64  -1:0]  lane_enable;
-    logic               is_last_line;
+    logic                           is_last_line;
 } mac_wfm_port;
 
 endpackage
